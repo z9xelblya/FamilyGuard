715c099b67d9c8ddcfd0445861f33a102ece79d2
Katharina Gross <katharina270907@gmail.com>
2025-07-10 17:13:03 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./familyguard.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
